9b1d2edb53c0948d51449c85cacfbcb83058e152
cchappellfleming <colin.chappell@flemingcollege.ca>
2022-02-03 10:34:02 -0500

    Cluster Zoom

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 5,
     center: {lat: 46.57873, lng: -82.723984},
   });
   // Create an array of alphabetical characters used to label the markers.
