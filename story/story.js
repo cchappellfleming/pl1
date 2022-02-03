72e2a14e3794013f7610866a67099c3abca672b7
cchappellfleming <colin.chappell@flemingcollege.ca>
2022-02-03 12:46:32 -0500

    Story 18

--- story/story.js
+++ story/story.js
@@ -1,17 +1,35 @@
-function initialize() {
+// function initialize() {
+//     const glacier = { lat: 63.5708, lng: -19.2924 };
+//     const path = {lat: 63.498410145945954, lng: -19.397552790404397}
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//       center: glacier,
+//       zoom: 10,
+//     });
+
+//     const panorama = new google.maps.StreetViewPanorama(
+//       document.getElementById("path"),
+//       {
+//         position: glacier
+//       }
+//     );
+//     map.setStreetView(panorama);
+//   }
+  function initialize() {
     const glacier = { lat: 63.5708, lng: -19.2924 };
-    const path = {lat: 63.498410145945954, lng: -19.397552790404397}
     const map = new google.maps.Map(document.getElementById("map"), {
       center: glacier,
-      zoom: 10,
+      zoom: 14,
     });
-
     const panorama = new google.maps.StreetViewPanorama(
-      document.getElementById("path"),
+      document.getElementById("pano"),
       {
-        position: glacier
+        position: glacier,
+        pov: {
+          heading: 34,
+          pitch: 10,
+        },
       }
     );
+  
     map.setStreetView(panorama);
   }
-  
